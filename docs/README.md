41db1e9c5cd1b56b8e900f77668d90bb3e268fa5
Naldo Wang <1165648703@qq.com>
2022-01-16 21:27:56 +0800

    初始化提交

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+---
+home: true
+title: Home Page
+date: 2022-01-12
+---
---
+++ vuepress.config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'zh-CN',
+  title: '个人博客',
+  description: `Naldo's Personal Blog`
+})
