c9c3b5290c766c8a762cc2b463323a567ec667e4
Naldo Wang <1165648703@qq.com>
2022-01-26 00:09:44 +0800

    修改base为'/'

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
-  base: '/blog/',
+  base: '/',
   lang: 'zh-CN',
   title: `Naldo's Blog`,
   description: `欢迎来到我的博客`
