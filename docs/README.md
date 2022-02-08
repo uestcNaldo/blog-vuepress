f0d6e130bb2b601f3be099244d980f6d24515cc2
Naldo Wang <1165648703@qq.com>
2022-02-08 08:50:50 +0800

    修改样式

--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,25 @@
 ---
-home: true
-title: 主页
 date: 2022-01-12
+title: 主页
+home: true
+heroText: 天然呆的呜啦啦
+tagline: null
+heroImage: /svg/chick.svg
 ---
 
-> 不积跬步无以至千里，不积小流无以成江海。🌊
+# 🍉Naldo的个人博客✨
 
-![Background](./assets/image/home_sky_moon.webp)
+> 好记性不如烂笔头。📝
+>
+> 不积跬步，无以至千里；不积小流，无以成江海。🌊
+
+## 希望能够记录一些被自己忽视的事情
+
+- 代码技术的**难点问题**、解决方案
+- 利用计算机技术解决生活中的需求
+- 对人生感悟的心得、排解心中烦恼
+
+## 博客的开发技术
+
+- vuepress 2.x beta
+- markdown
--- vuepress.config.ts
+++ vuepress.config.ts
@@ -5,5 +5,21 @@ export default defineUserConfig<DefaultThemeOptions>({
   base: '/',
   lang: 'zh-CN',
   title: `Naldo's Blog`,
-  description: `欢迎来到我的博客`
+  description: `欢迎来到我的博客`,
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: '/svg/chick.svg',
+    navbar: [
+      { text: '主页', link: '/' },
+      {
+        text: '回忆',
+        children: [
+          { text: '1', link: '/memory' }
+        ]
+      }
+    ]
+  }
 })
